769735742dcf32e8f65442342585aab4a094882e
Andrii <akornytskyi@brainence.com>
2018-10-04 13:25:47 +0300

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/web/index.web.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.web.js', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
